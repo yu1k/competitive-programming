85a1a904dac53a946f75e437f5c4e7fd51833d64
yu1k <48705423+yu1k@users.noreply.github.com>
2019-10-29 11:43:23 +0900

    ABC002のA問題を解くために書いたコード

---
+++ abc002/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int X;
+    int Y;
+    cin >> X >> Y;
+    if(X < Y){
+        cout << Y << endl;
+    }
+    else{
+        cout << X << endl;
+    }
+}
