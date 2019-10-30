04713104adc4c59388c9d16cf1b7f4e91ec3b4b3
yu1k <48705423+yu1k@users.noreply.github.com>
2019-10-30 09:49:46 +0900

    A問題-世界のFizzBuzz

---
+++ abc006/a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    if(N % 3 == 0){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}
