446f00e0a83a8434254aa7c3e8a6f9c3ba998a01
yu1k <48705423+yu1k@users.noreply.github.com>
2019-10-30 10:04:31 +0900

    A問題 - AtCoder社の給料

--- abc003/a.cpp
+++ abc003/a.cpp
@@ -5,5 +5,5 @@ using namespace std;
 int main(){
     int N;
     cin >> N;
-    cout << (1 + N) * 10000 / 2 << endl;  //1+Nの真ん中の値をとる。足し算して割る。
+    cout << (1 + N) * 10000 / 2 << endl;  //1+Nの真ん中の値をとる。足し算して割る
 }
