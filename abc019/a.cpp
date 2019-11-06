2c0694a38800d7b7361b49f782fec180d1dfc673
yu1k <48705423+yu1k@users.noreply.github.com>
2019-11-06 15:46:13 +0900

    A問題 - 高橋くんと年齢

--- abc019/a.cpp
+++ abc019/a.cpp
@@ -1,3 +1,4 @@
+//解説通りの実装
 #include <iostream>
 #include <algorithm>
 
@@ -10,5 +11,23 @@ int main(){
     cin >> a >> b >> c;
     int S[3] = {a, b, c};
     sort(S, S+3);
+    cout << S[1] << endl;
+}
+
+
+//先生が教えてくれた実装
+//こっちの方が応用がききそう
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    vector<int> S(3);
+    for(int i = 0; i < 3; i++){
+        cin >> S[i];
+    }
+    sort(S.begin(), S.end());
     cout << S[1] << endl;
 }
