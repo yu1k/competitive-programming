adb64a317f7835b739fd0f931ad1473bf9f51dab
yu1k <48705423+yu1k@users.noreply.github.com>
2019-11-06 15:49:36 +0900

    A問題 - クイズ

---
+++ abc020/a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int Q;
+    cin >> Q;
+    if(Q == 1){
+        cout << "ABC" << endl;
+    }
+    else if(Q == 2){
+        cout << "chokudai" << endl;
+    }
+}
