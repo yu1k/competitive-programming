1c2fc0632495ad096272af77f8e72252fcc3dbcd
yu1k <48705423+yu1k@users.noreply.github.com>
2019-11-09 23:25:03 +0900

    A問題 - Sum of Two Integers

---
+++ Nikkei-Programming-King-Final-Match-Qualifier-2nd/a.cpp
@@ -0,0 +1,10 @@
+//ディレクトリ名はGoogle翻訳で(第二回全国統一プログラミング王決定戦予選)を翻訳したのでなんかおかしい英語になってるかもです
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    cout << (N - 1) / 2 << endl;
+}
