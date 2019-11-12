318992054eb4d0f5564354d67bf1cfb2cec85cc2
yu1k <48705423+yu1k@users.noreply.github.com>
2019-11-12 17:07:44 +0900

    A問題 - テスト評価

---
+++ abc028/a.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    if(0 <= N && N <= 59){
+        cout << "Bad" << endl;
+    }
+    else if(60 <= N && N <= 89){
+        cout << "Good" << endl;
+    }
+    else if(90 <= N && N <= 99){
+        cout << "Great" << endl;
+    }
+    else if(100 == N){
+        cout << "Perfect" << endl;
+    }
+    return 0;
+}
