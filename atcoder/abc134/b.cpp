1f03fec721b886f9256cdd7e23ad17624f928acc
yu1k <48705423+yu1k@users.noreply.github.com>
2019-11-15 13:42:49 +0900

    B - Golden Apple

--- atcoder/abc134/b.cpp
+++ atcoder/abc134/b.cpp
@@ -7,6 +7,6 @@ int main(){
     int D;
     cin >> N >> D;
     //N,Dの順で標準入力
-        cout << (N + D * 2) / (D * 2 +1) << endl;
-        //N + 2D / 2D +1を計算して出力.
+    cout << (N + D * 2) / (D * 2 +1) << endl;
+    //N + 2D / 2D +1を計算して出力.
 }
