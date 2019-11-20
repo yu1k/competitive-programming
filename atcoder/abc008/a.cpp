74a0453832ef496fb247a50e6cd00d85b9d87e15
yu1k <48705423+yu1k@users.noreply.github.com>
2019-11-20 15:44:59 +0900

    A - アルバム

--- atcoder/abc008/a.cpp
+++ atcoder/abc008/a.cpp
@@ -8,3 +8,16 @@ int main(){
     cin >> S >> T;
     cout << T - S + 1 << endl;
 }
+
+
+//別の解き方
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int S;
+    int T;
+    cin >> S >> T;
+    cout << T - (S - 1) << endl;
+}
