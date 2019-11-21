8156f25d74cb22f409ca41c2da7400662c4a98e6
yu1k <48705423+yu1k@users.noreply.github.com>
2019-11-21 23:15:10 +0900

    A - お茶

--- atcoder/abc038/a.cpp
+++ atcoder/abc038/a.cpp
@@ -12,3 +12,23 @@ int main(){
         cout << "NO" << endl;
     }
 }
+
+
+//別の解法
+//.back()を使って実装.
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    string S;
+    cin >> S;
+    if(S.back() == 'T'){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}
