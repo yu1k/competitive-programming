eb7bc057f6ff69eadac0010138604da4d101d981
yuik <48705423+yu1k@users.noreply.github.com>
2019-12-06 23:56:02 +0900

    A - AtCoder *** Contest

---
+++ abc048/a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string AtCoder;
+    string s;
+    string Contest;
+    cin >> AtCoder >> s >> Contest;
+    cout << AtCoder[0] << s[0] << Contest[0] << endl;
+}
