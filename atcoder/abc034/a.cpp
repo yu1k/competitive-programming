fe71676769267a21cc64f045c721a04991cdb38b
yuik <48705423+yu1k@users.noreply.github.com>
2019-12-25 19:18:55 +0900

    A - テスト

---
+++ atcoder/abc034/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    int y;
+    cin >> x >> y;
+    if(x < y){
+        cout << "Better" << endl;
+    }
+    else{
+        cout << "Worse" << endl;
+    }
+}
