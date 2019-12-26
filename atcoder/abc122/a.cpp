16a03533c9d6bd0c3c27f7d4eca816e7680b3e3d
yuik <48705423+yu1k@users.noreply.github.com>
2019-12-26 22:55:51 +0900

    A - Double Helix

---
+++ atcoder/abc122/a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char b;
+    cin >> b;
+    if(b == 'A') cout << 'T' << endl;
+    if(b == 'T') cout << 'A' << endl;
+    if(b == 'C') cout << 'G' << endl;
+    if(b == 'G') cout << 'C' << endl;
+}
