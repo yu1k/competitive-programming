be350112e716c5f31ed19509a23bc517dcb712a2
yuik <48705423+yu1k@users.noreply.github.com>
2020-01-03 23:33:32 +0900

    A - Still TBD

---
+++ atcoder/abc119/a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int y;
+    int m;
+    int d;
+    char a;
+    char b;
+    cin >> y >> a >> m >> b >> d;
+    if(4 < m){
+        cout << "TBD" << endl;
+    }
+    else{
+        cout << "Heisei" << endl;
+    }
+}
