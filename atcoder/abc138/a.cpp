b4c08046ee3e5a8020c2e6e7c8e7c736a25383bc
yuik <48705423+yu1k@users.noreply.github.com>
2020-01-15 23:39:47 +0900

    A - Red or Not

---
+++ atcoder/abc138/a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a; 
+    string s;
+    cin >> a;
+    cin >> s;
+    /*for(int i = 1; i < a; i++){*/
+        if(a >= 3200){
+            cout << s << endl;
+        }else if(a < 3200){
+            cout << "red" << endl;
+        }
+    }
