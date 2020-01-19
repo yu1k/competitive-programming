927a42e42615e0fe1ce7620f3cea0d9f7676382d
yuik <48705423+yu1k@users.noreply.github.com>
2020-01-19 22:34:56 +0900

    A - AC or WA

---
+++ atcoder/abc152/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    int m;
+    cin >> n >> m;
+    if(n == m){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
+    }
+}
