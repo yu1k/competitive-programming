4d1b3f8e05c820814392278af46e597fa8643cc2
yuik <48705423+yu1k@users.noreply.github.com>
2020-01-20 11:43:59 +0900

    A - 台形 / Trapezoids

---
+++ atcoder/abc045/a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int h;
+    cin >> a;
+    cin >> b;
+    cin >> h;
+    cout << (a + b) * h / 2  << endl;
+}
