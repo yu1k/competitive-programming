35e39d0fb4951ca9e7eba75ce5251b48bd7fe138
yuik <48705423+yu1k@users.noreply.github.com>
2020-01-22 16:47:38 +0900

    C++ / テンプレート

--- template.cpp
+++ template.cpp
@@ -2,7 +2,11 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 #include <cmath>
+
+#define ll long long 
+#define rep(i, n) for(int i = 0; i < n; i++)
 
 using namespace std;
 
@@ -23,6 +27,17 @@ cout << max({a, b, c}) << endl;
 cout << min({a, b, c}) << endl;
 
 
+//絶対値を取得
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+    cout << abs(a) << endl;
+}
+
 
 //charインクリメントできる
 #include <iostream>
@@ -37,7 +52,7 @@ int main(){
 }
 
 
-
+//昇順ソート
 //入力された3つの数字の中央値を出力するコード/ソートのテンプレート
 //https://atcoder.jp/contests/abc019/tasks/abc019_1
 #include <iostream>
@@ -55,6 +70,39 @@ int main(){
     cout << S[1] << endl;
 }
 
+
+//降順ソート
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    vector<int> a(5);
+    for(int i = 0; i < 5; i++){
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    reverse(a.begin(), a.end());
+    cout << a[0] << endl;
+}
+
+//sort();が1行で書ける降順ソート
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    vector<int> a(5);
+    for(int i = 0; i < 5; i++){
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end(), greater<int>());
+    cout << a[0] << endl;
+}
 
 //repマクロを使ってみた
 #include <iostream>
