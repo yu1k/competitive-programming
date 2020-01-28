3898f8b0658c56f37b4a0fa8ce07776f60efaf49
yuik <48705423+yu1k@users.noreply.github.com>
2020-01-28 11:58:56 +0900

    B - コンテスト

---
+++ JOI2008-2009/qualifying/b.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    vector<int> w(10);
+    vector<int> k(10);
+    for(int i = 0; i < 10; i++){
+        cin >> w[i];
+    }
+    for(int i = 0; i < 10; i++){
+        cin >> k[i];
+    }
+    sort(w.begin(), w.end(), greater<int>());
+    sort(k.begin(), k.end(), greater<int>());
+
+    cout << w[0] + w[1] + w[2] << " ";
+    cout << k[0] + k[1] + k[2] << endl;
+}
