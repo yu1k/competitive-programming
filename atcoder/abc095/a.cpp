4b2874746d5ec6664650ce0a194eac6ca6c11909
yuik <48705423+yu1k@users.noreply.github.com>
2020-01-31 12:31:19 +0900

    A - Something on It

---
+++ atcoder/abc095/a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    int ramen = 700;
+    int egg = 100;
+    int roasted = 100;
+    int green_onions = 100;
+    int result = 700;
+    if(s[0] == 'o') result = result + egg;
+    if(s[1] == 'o') result = result + roasted;
+    if(s[2] == 'o') result = result + green_onions;
+    cout << result << endl;
+}
