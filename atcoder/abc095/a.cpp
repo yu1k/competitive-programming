6f5de5de1d1884faa068bd9f5040628196a232ca
yuik <48705423+yu1k@users.noreply.github.com>
2020-02-01 23:45:23 +0900

    A - Something on It

--- atcoder/abc095/a.cpp
+++ atcoder/abc095/a.cpp
@@ -11,6 +11,7 @@ int main(){
     int roasted = 100;
     int green_onions = 100;
     int result = 700;
+
     if(s[0] == 'o') result = result + egg;
     if(s[1] == 'o') result = result + roasted;
     if(s[2] == 'o') result = result + green_onions;
