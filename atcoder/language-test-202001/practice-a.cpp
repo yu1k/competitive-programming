adf617a989f401c4bcab7dc53e580f0fea3003a5
yuik <48705423+yu1k@users.noreply.github.com>
2020-02-02 22:18:48 +0900

    PracticeA - Welcome to AtCoder

---
+++ atcoder/language-test-202001/practice-a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int c;
+    string s;
+    cin >> a;
+    cin >> b >> c;
+    cin >> s;
+    cout << a + b + c << " " << s << endl;
+}
