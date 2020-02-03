7a189c4643433953b1ef5700c7e788203dba3739
yuik <48705423+yu1k@users.noreply.github.com>
2020-02-03 20:21:37 +0900

    A - レシート

--- JOI2009-2010/qual/a.cpp
+++ JOI2009-2010/qual/a.cpp
@@ -5,10 +5,12 @@ using namespace std;
 int main(){
     int result;
     int book;
+    for(int i = 0; i < 5; i++){
     cin >> result; //総額
     for(int i = 0; i < 9; i++){
         cin >> book;
         result = result - book;
     }
     cout << result << endl;
+    }
 }
