2ee0fd23610897590e403fdb4ed53f78fa293d96
yuik <48705423+yu1k@users.noreply.github.com>
2020-02-04 23:58:14 +0900

    A - ringring

---
+++ atcoder/abc066/a-sort.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    vector<int> n(3);
+    for(int i = 0; i < 3; i++){
+        cin >> n[i];
+    }
+    sort(n.begin(), n.end());
+    cout << n[0] + n[1] << endl;
+}
