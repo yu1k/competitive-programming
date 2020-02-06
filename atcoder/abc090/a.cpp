692462170b50cbb9310340d94488b212a39af57f
yuik <48705423+yu1k@users.noreply.github.com>
2020-02-06 23:49:31 +0900

    A - Diagonal String

---
+++ atcoder/abc090/a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char a;
+    char b;
+    char c;
+    char a2;
+    char b2;
+    char c2;
+    char a3;
+    char b3;
+    char c3;
+    cin >> a >> b >> c;
+    cin >> a2 >> b2 >> c2;
+    cin >> a3 >> b3 >> c3;
+    cout << a << b2 << c3 << endl;
+}
