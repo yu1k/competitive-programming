b095c5f07c20dc2f5157679eb90e551e5c2f5761
yuik <48705423+yu1k@users.noreply.github.com>
2020-02-10 17:54:20 +0900

    A - 合計時間 (Total Time)

---
+++ JOI2010-2011/qual/a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int c;
+    int d;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cout << (a + b + c + d) / 60 << endl;
+    cout << (a + b + c + d) % 60 << endl;
+}
