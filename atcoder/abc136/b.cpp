a366b2793e42f16080ac349566b32f8e37278f00
yuik <48705423+yu1k@users.noreply.github.com>
2020-02-14 23:16:36 +0900

    B - Uneven Numbers

---
+++ atcoder/abc136/b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int result = 0;
+    for(int i = 1; i < n + 1; i++){
+        if(to_string(i).length() % 2 == 1){
+            result = result + 1;
+        }
+    }
+    cout << result << endl;
+}
