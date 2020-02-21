9fa53ba3ac6931b6403be30821f3e0bcb10bc431
yuik <48705423+yu1k@users.noreply.github.com>
2020-02-21 23:59:32 +0900

    A - 居合を終え、青い絵を覆う

---
+++ atcoder/abc049/a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char = c;
+    cin >> c;
+    if(c == ('a' || 'e' || 'i' || 'o' || 'u'){
+        cout << "vowel" << endl;
+    }
+    else{
+        cout << "consonant" << endl;
+    }
+}
