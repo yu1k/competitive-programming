7b6904deaab76864866bf5d460277830ad62c395
yuik <48705423+yu1k@users.noreply.github.com>
2020-03-05 08:18:58 +0900

    A - AtCoder Beginner Contest 999

---
+++ atcoder/abc111/a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    for(int i = 0; i < 3; i++){
+     	char n;
+    	cin >> n;
+        if(n == '1'){
+            cout << 9;
+        }
+        else{
+            cout << 1;
+        }
+    }
+    cout << endl;
+}
