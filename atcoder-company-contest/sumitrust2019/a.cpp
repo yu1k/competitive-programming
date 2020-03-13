a1c1a3252ac8b6faa07243c635dc3cce931f2eeb
yuik <48705423+yu1k@users.noreply.github.com>
2020-03-13 23:06:14 +0900

    A - November 30

---
+++ atcoder-company-contest/sumitrust2019/a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int m1;
+    int m2;
+    int d1;
+    int d2;
+    cin >> m1 >> d1;
+    cin >> m2 >> d2;
+    if(d2 == 1){
+        cout << "1" << endl;
+    }
+    else{
+        cout << "0" << endl;
+    }
+}
