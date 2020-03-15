120dfb60df83d546adeae3d6c0ea8bac712b6a57
yuik <48705423+yu1k@users.noreply.github.com>
2020-03-15 23:40:46 +0900

    A - Parking

---
+++ atcoder/abc080/a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    int a;
+    int b;
+    cin >> n >> a >> b;
+    if(n * a < b){
+        cout << n * a << endl;
+    }
+    else{
+        cout << b << endl;
+    }
+}
