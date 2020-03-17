ca7b918b9c75d76c012f0441e7115a057a86158d
yuik <48705423+yu1k@users.noreply.github.com>
2020-03-17 23:15:06 +0900

    add : char-search

--- algorithm/char-search.cpp
+++ algorithm/char-search.cpp
@@ -1,6 +1,7 @@
 /***   探索   ***
 文字列の中から特定の文字があった場合, 
 その文字が何個あったかをカウントして出力するプログラム*/
+/*  サンプル
 #include <iostream>
 #include <string>
 
@@ -16,4 +17,26 @@ int main(){
         }
     }
     cout << result << endl;
+}
+*/
+/***
+ * 文字列strにcで入力した文字が何個含まれているかカウントし、
+   数を出力するプログラム                                 ***/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string str;
+	cin >> str;
+	int result = 0;
+	char c;
+	cin >> c;
+	for(int i = 0; i < str.length(); i++){
+		if(str[i] == c){
+			result++;
+		}
+	}
+	cout << result << endl;
 }
