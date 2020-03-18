935c9d390da3a9bcecc03ea3a58733a20fbdcebb
yuik <48705423+yu1k@users.noreply.github.com>
2020-03-19 00:44:47 +0900

    B - エージェント高橋君

---
+++ atcoder/abc039/b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+    int n = 0;
+    int result = sqrt(sqrt(x));
+    cout << result << endl;
+}
