284b3d89864b6535105cc7d86b19ee0ca3421a21
yuik <48705423+yu1k@users.noreply.github.com>
2020-03-23 14:33:24 +0900

    A - The Number of Even Pairs

--- atcoder/abc159/a.cpp
+++ atcoder/abc159/a.cpp
@@ -1,17 +1,4 @@
-/* This code is written by yuiky*/
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <cmath>
-#include <cctype>
-
-#define mod 1000000007
-#define ab 123456789
-#define ba 987654321
-#define ll long long 
-#define rep(i, n) for(int i = 0; i < n; i++)
-#define str(i, s) for(int i = 0; i < s.length(); i++)
 
 using namespace std;
 
@@ -19,10 +6,5 @@ int main(){
     int n;
     int m;
     cin >> n >> m;
-    if(n % 2 == 1 && ){
-
-    }
-    else{
-        cout << "0" << endl;
-    }
+    cout << n * (n - 1) / 2 + m * (m - 1) / 2 << endl;
 }
