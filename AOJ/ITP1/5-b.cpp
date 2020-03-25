526bc830670aa74596cc9a4c9b99ec1e99ea14b9
yuik <48705423+yu1k@users.noreply.github.com>
2020-03-25 15:15:51 +0900

    ITP1_5_B: Print a Frame

---
+++ AOJ/ITP1/5-b.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int h;
+    int w;
+    for(int i = 0; i < 300;){
+        cin >> h >> w;
+        if(h == 0 && w == 0){
+            break;
+        }
+        for(int i = 0; i < h; i++){
+            for(int j = 0; j < w; j++){
+                if(i == 0 || j == 0 || i == h - 1 || j == w - 1){
+                    cout << "#";
+                }
+                else{
+                    cout << ".";
+                }
+            }
+            cout << "" << endl;
+        }
+        cout << "" << endl;
+    }
+}
