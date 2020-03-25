0b508f333af83143e7a69a99c84725aac18a5a7d
yuik <48705423+yu1k@users.noreply.github.com>
2020-03-25 15:32:23 +0900

    ITP1_5_C: Print a Chessboard

---
+++ AOJ/ITP1/5-c.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int h;
+    int w;
+    for(int i = 0; i < 300; i++){
+        cin >> h >> w;
+        if(h == 0 && w == 0){
+            break;
+        }
+        for(int i = 0; i < h; i++){
+            for(int j = 0; j < w; j++){
+                if((i + j) % 2 == 0){
+                    cout << "#";
+                }
+                else{
+                    cout << ".";
+                }
+            }
+            cout << "" << endl;
+        }
+        cout << "" << endl;
+    }
+}
