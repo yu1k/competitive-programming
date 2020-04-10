c5664b08d1e706244d5751535afa819d9f3860c2
yuik <48705423+yu1k@users.noreply.github.com>
2020-04-10 23:44:09 +0900

    B - OddString

---
+++ atcoder/abc072/b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    string result;
+    for(int i = 0; i < s.length(); i++){
+        if(i % 2 == 0){
+            cout << s[i];
+        }
+    }
+    cout << "" << endl;
+}
