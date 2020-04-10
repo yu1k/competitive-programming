226a99a8a370e5bea63b0db0ad47f06d1330c197
yuik <48705423+yu1k@users.noreply.github.com>
2020-04-10 23:43:34 +0900

    A - Sandglass2

---
+++ atcoder/abc072/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    int t;
+    cin >> x >> t;
+    if(x > t){
+        cout << x - t << endl;
+    }
+    else{
+        cout << "0" << endl;
+    }
+}
