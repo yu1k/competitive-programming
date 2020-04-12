d9cc5c3c331f573332df9de6058a6b5ac35ed9b3
yuik <48705423+yu1k@users.noreply.github.com>
2020-04-12 23:00:48 +0900

    A - Lucky 7

---
+++ atcoder/abc162/a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    string n;
+    cin >> n;
+    if(n[0] == '7' || n[1] == '7' || n[2] == '7'){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
+    }
+}
