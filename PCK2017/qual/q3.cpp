146758de1ff5ffef8bc582c4f8f2ab42a9e3a4aa
yu1k <48705423+yu1k@users.noreply.github.com>
2020-04-27 18:34:21 +0900

    問題3 - 9月X日

--- PCK2017/qual/q3.cpp
+++ PCK2017/qual/q3.cpp
@@ -7,5 +7,5 @@ int main(){
     int x;
     cin >> x;
     string day[7] = {"mon", "tue", "wed", "thu", "fri", "sat", "sun"};
-    cout << day[(x + 3) % 7] << endl;
+    cout << day[(x + 3) % 7] << endl; 
 }
