c0baf43cb9f21220a1d42e468b5f83048d4e3c7d
yu1k <48705423+yu1k@users.noreply.github.com>
2020-05-04 07:11:22 +0900

    add: 合計(整数の和)

---
+++ algorithm/sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+        sum += a[i];
+    }
+    cout << sum << endl;
+}
