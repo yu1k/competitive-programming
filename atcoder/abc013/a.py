ee80374032992a44e5c30361f53a978b31235ac1
yu1k <48705423+yu1k@users.noreply.github.com>
2020-05-07 23:42:03 +0900

    A - A

---
+++ atcoder/abc013/a.py
@@ -0,0 +1,16 @@
+x = input()
+
+result = 0
+
+if x == 'A':
+    result = 1
+elif x == 'B':
+    result = 2
+elif x == 'C':
+    result = 3
+elif x == 'D':
+    result = 4
+elif x == 'E':
+    result = 5
+
+print(result)
