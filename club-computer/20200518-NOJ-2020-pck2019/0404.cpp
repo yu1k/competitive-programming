eb304f7d37b84e7e87dd064a60d986ad664376c5
yu1k <48705423+yu1k@users.noreply.github.com>
2020-05-22 18:18:28 +0900

    問題4 - 柴犬の数

---
+++ club-computer/20200518-NOJ-2020-pck2019/0404.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int r;
+    int b;
+    int w;
+    int g;
+    cin >> r >> b >> w >> g;
+    cout << r + b + w + g << endl;
+    return 0;
+}
