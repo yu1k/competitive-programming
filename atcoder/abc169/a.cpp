2073cd5912338d26a3f1793cbe033163ef9cfaee
yu1k <48705423+yu1k@users.noreply.github.com>
2020-06-02 18:18:00 +0900

    A - Multiplication 1

---
+++ atcoder/abc169/a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << a * b << endl;
+    return 0;
+}
