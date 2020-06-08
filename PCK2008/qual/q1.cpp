91573a3607a76fdbd7b085be41a9a9e1d34da949
yu1k <48705423+yu1k@users.noreply.github.com>
2020-06-08 17:43:01 +0900

    create: README

---
+++ PCK2008/qual/q1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    vector<string> name(9);
+    vector<int> a(9);
+    vector<int> b(9);
+    for(int i = 0; i < 9; i++){
+        cin >> name[i] >> a[i] >> b[i];
+    }
+    for(int i = 0; i < 9; i++){
+        cout << name[i] << " " << a[i] + b[i] << " " << (a[i] * 200) + (b[i] * 300) << endl;
+    }
+    return 0;
+}
