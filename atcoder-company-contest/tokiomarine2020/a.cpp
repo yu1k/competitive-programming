baf2bb92c5b18be61a3f606a53b0fe942639cfa7
yu1k <48705423+yu1k@users.noreply.github.com>
2020-06-13 23:08:48 +0900

    A - Nickname

---
+++ atcoder-company-contest/tokiomarine2020/a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    cout << s[0] << s[1] << s[2] << endl;
+    return 0;
+}
