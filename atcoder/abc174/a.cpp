41d509bb2d9c1690da547d8784cc6db2e1506e3c
yu1k <48705423+yu1k@users.noreply.github.com>
2020-08-02 23:03:22 +0900

    A - Air Conditioner

---
+++ atcoder/abc174/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+    if(30 <= x){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
+    }
+    return 0;
+}
