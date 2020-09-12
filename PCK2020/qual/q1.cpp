70069343451e459a00142ea7f87cd2009a04e860
yu1k <48705423+yu1k@users.noreply.github.com>
2020-09-12 23:56:16 +0900

    問題1 - 緯度経度

---
+++ PCK2020/qual/q1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+​
+using namespace std;
+​
+int main(){
+    int la;
+    int lo;
+    cin >> la >> lo;
+    int laresult = la / 3600;
+    int loresult = lo / 3600;
+    cout << laresult << " " << loresult << endl;
+    return 0;
+}
