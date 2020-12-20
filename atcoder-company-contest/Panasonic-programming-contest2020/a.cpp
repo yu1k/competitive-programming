cd268c73a4eed8479e807799748626487d98ac36
yu1k <48705423+yu1k@users.noreply.github.com>
2020-12-20 22:11:00 +0900

    A - Brick

---
+++ atcoder-company-contest/Panasonic-programming-contest2020/a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int n;
+    int w;
+    cin >> n >> w;
+    int result = (n / w);
+    cout << result << endl;
+    return 0;
+}
