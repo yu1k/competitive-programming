b17f02524f342ec56794f97ee2dfdc765295d8df
yuik <48705423+yu1k@users.noreply.github.com>
2021-08-24 18:10:21 +0900

    問題1 - 柴犬の数

---
+++ PCK2019/qual/q1.cpp
@@ -0,0 +1,10 @@
+// https://onlinejudge.u-aizu.ac.jp/beta/review.html#n_online_judge_2021_pck2019re/5818996
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    int a; int b; int c; int d;
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+    int result = a + b + c + d;
+    printf("%d\n", result);
+    return 0;
+}
